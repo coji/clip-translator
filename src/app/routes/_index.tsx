7b2ba858d5c4ae63e001f380c30cc8f2dc4719b6
coji <coji@techtalk.jp>
2024-03-22 21:01:48 +0900

    Update tauri.conf.json and add test route

--- src/app/routes/_index.tsx
+++ src/app/routes/_index.tsx
@@ -1,8 +1,13 @@
+import { Link } from '@remix-run/react'
+
 export default function IndexPage() {
   return (
     <div>
-      <div>Hello Tauri!</div>
+      <h1 className="text-4xl font-bold">Hello Remix SPA mode on Tauri!</h1>
       <img src="/tauri.svg" alt="tauri" />
+      <Link to="/test" className="text-blue-500 underline">
+        Test Page
+      </Link>
     </div>
   )
 }
