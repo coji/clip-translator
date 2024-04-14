b6cbb66b878e3d6e506b0ffd7fd35e8ee2beb4bb
coji <coji@techtalk.jp>
2024-04-14 19:56:17 +0900

    Refactor UI components and update dependencies

--- app/routes/config/route.tsx
+++ app/routes/config/route.tsx
@@ -81,120 +81,119 @@ export default function ConfigPage() {
   const modelControl = useInputControl(fields.model)
 
   return (
-    <ConfigPageLayout asChild>
-      <Form method="POST" {...getFormProps(form)}>
-        <ConfigHeader>
-          <ConfigTitle>Configurations</ConfigTitle>
-        </ConfigHeader>
+    <ConfigPageLayout>
+      <Form method="POST" {...getFormProps(form)} />
+      <ConfigHeader>
+        <ConfigTitle>Configurations</ConfigTitle>
+      </ConfigHeader>
 
-        <ConfigContent>
-          {/* anthropic  */}
-          <ConfigField>
-            <Label htmlFor={fields.anthropic_api_key.id}>
-              Anthropic API Key
-            </Label>
-            <Input
-              {...getInputProps(fields.anthropic_api_key, {
-                type: 'password',
-              })}
-            />
-            <div
-              id={fields.anthropic_api_key.errorId}
-              className="text-sm text-destructive"
-            >
-              {fields.anthropic_api_key.errors}
-            </div>
-          </ConfigField>
+      <ConfigContent>
+        {/* anthropic  */}
+        <ConfigField>
+          <Label htmlFor={fields.anthropic_api_key.id}>Anthropic API Key</Label>
+          <Input
+            {...getInputProps(fields.anthropic_api_key, {
+              type: 'password',
+            })}
+          />
+          <div
+            id={fields.anthropic_api_key.errorId}
+            className="text-sm text-destructive"
+          >
+            {fields.anthropic_api_key.errors}
+          </div>
+        </ConfigField>
 
-          <ConfigField>
-            <Label htmlFor={fields.model.id}>Model</Label>
-            <Select
-              defaultValue={fields.model.initialValue}
-              onValueChange={modelControl.change}
-              onOpenChange={(open) => {
-                if (!open) modelControl.blur()
-              }}
+        <ConfigField>
+          <Label htmlFor={fields.model.id}>Model</Label>
+          <Select
+            defaultValue={fields.model.initialValue}
+            onValueChange={modelControl.change}
+            onOpenChange={(open) => {
+              if (!open) modelControl.blur()
+            }}
+          >
+            <SelectTrigger
+              form={form.id}
+              name={fields.model.name}
+              id={fields.model.id}
+              value={fields.model.value}
+              className="capitalize"
             >
-              <SelectTrigger
-                name={fields.model.name}
-                id={fields.model.id}
-                className="capitalize"
-              >
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent>
-                {Object.keys(Models).map((modelId) => (
-                  <SelectItem
-                    key={modelId}
-                    value={modelId}
-                    className="capitalize"
-                  >
-                    {modelId}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-            <div id={fields.model.errorId} className="text-sm text-destructive">
-              {fields.model.errors}
-            </div>
-          </ConfigField>
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              {Object.keys(Models).map((modelId) => (
+                <SelectItem
+                  key={modelId}
+                  value={modelId}
+                  className="capitalize"
+                >
+                  {modelId}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <div id={fields.model.errorId} className="text-sm text-destructive">
+            {fields.model.errors}
+          </div>
+        </ConfigField>
 
-          <ConfigField className="flex-1">
-            <Label
-              className="block"
-              id={fields.system_prompt.descriptionId}
-              htmlFor={fields.system_prompt.id}
-            >
-              System Prompt
-            </Label>
-            <Textarea
-              className="flex-1"
-              {...getTextareaProps(fields.system_prompt)}
-            />
-            <div
-              id={fields.system_prompt.errorId}
-              className="text-sm text-destructive"
-            >
-              {fields.system_prompt.errors}
-            </div>
-          </ConfigField>
-        </ConfigContent>
+        <ConfigField className="flex-1">
+          <Label
+            className="block"
+            id={fields.system_prompt.descriptionId}
+            htmlFor={fields.system_prompt.id}
+          >
+            System Prompt
+          </Label>
+          <Textarea
+            className="flex-1"
+            {...getTextareaProps(fields.system_prompt)}
+          />
+          <div
+            id={fields.system_prompt.errorId}
+            className="text-sm text-destructive"
+          >
+            {fields.system_prompt.errors}
+          </div>
+        </ConfigField>
+      </ConfigContent>
 
-        <ConfigFooter>
-          <Button type="button" variant="ghost" asChild>
-            <Link to={$path('/')}>Cancel</Link>
+      <ConfigFooter>
+        <Button type="button" variant="ghost" asChild>
+          <Link to={$path('/')}>Cancel</Link>
+        </Button>
+        <Button form={form.id} type="submit" disabled={!form.dirty}>
+          Save
+        </Button>
+      </ConfigFooter>
+
+      {blocker.state === 'blocked' && (
+        <BlockerAlert>
+          <div className="flex-1">
+            Are you sure you want to leave without saving?
+          </div>
+
+          <Button
+            type="button"
+            variant="ghost"
+            size="sm"
+            onClick={() => blocker.reset()}
+          >
+            Stay
           </Button>
-          <Button type="submit" disabled={!form.dirty}>
-            Save
+
+          <Button
+            type="button"
+            variant="secondary"
+            size="sm"
+            onClick={() => blocker.proceed()}
+          >
+            Continue
           </Button>
-        </ConfigFooter>
-
-        {blocker.state === 'blocked' && (
-          <BlockerAlert>
-            <div className="flex-1">
-              Are you sure you want to leave without saving?
-            </div>
-
-            <Button
-              type="button"
-              variant="ghost"
-              size="sm"
-              onClick={() => blocker.reset()}
-            >
-              Stay
-            </Button>
-
-            <Button
-              type="button"
-              variant="secondary"
-              size="sm"
-              onClick={() => blocker.proceed()}
-            >
-              Continue
-            </Button>
-          </BlockerAlert>
-        )}
-      </Form>
+        </BlockerAlert>
+      )}
     </ConfigPageLayout>
   )
 }
