419cecfb8994d055ae969c70fdac504aab279d51
coji <coji@techtalk.jp>
2025-02-16 14:33:52 +0900

    refactor: standardize import statements and remove trailing newline in package.json

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router'
 import { Toaster } from '~/components/ui'
 import { useGlobalShortcut } from './services/global-shortcut.client'
 import './styles/globals.css'
